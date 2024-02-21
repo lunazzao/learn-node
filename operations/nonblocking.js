78e105bc7fc57e1b91c38401b431458ee33c8b79
lunazzao <zhao.jiayu@northeastern.edu>
2024-02-21 16:26:51 -0500

    Activity Feb21

--- operations/nonblocking.js
+++ operations/nonblocking.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 function moreWork() {
     console.log('more work');
 }
-
+// asynchronies
 fs.readFile("data/test.txt", (err, data) => {
   if (err) console.log(err.message);
   else console.log(data);
